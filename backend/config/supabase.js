7c09c456534f7197519df6c8668605128761202f
subash <subashvelusamy10@gmail.com>
2026-04-05 21:53:06 +0530

    Remove hardcoded keys

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  "https://cwvehpwdsliyzanqllwu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3dmVocHdkc2xpeXphbnFsbHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxOTU3OTksImV4cCI6MjA4OTc3MTc5OX0.Yro8Dp7voYyTlYVXsDh8ZhzOmbxkY9KwpxzE1YLN60U"
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 module.exports = supabase;
